da9e1a58a9519c980ba88caf573c7e4354c64c76
ayman f. ali <ayman4swd@gmail.com>
2025-09-21 08:20:30 +0300

    perf: Add Redis caching for article and comments

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ArticleController extends Controller
 {
     public function show(Article $article)
     {
-        $article = Article::with('comments.user')->findOrFail($article->id);
+        $cacheKey = 'article_' . $article->id;
+
+        $article = Cache::remember($cacheKey, 60, function () use ($article) {
+            return Article::with('comments.user')->find($article->id);
+        });
 
         return view('articles.show', compact('article'));
     }
